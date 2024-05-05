26b21563403cfc349a6f0e9895ac26dc5c43b2d6
Bishesh Agarwal <bisheshagarwal1999@gmail.com>
2024-05-05 13:43:07 +0530

    Setup backend for deployment to render. Add a build script in package.json

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path")
 
 const connectDB = require("./config/db");
 const ProductRoutes = require("./routes/ProductRoutes.js");
@@ -33,8 +34,23 @@ app.get("/api/config/paypal", (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
+const __dirname = path.resolve();
 //Use uploads folder as static for file uploads
 app.use("/uploads", express.static("uploads"));
+
+if (process.env.NODE_ENV === "production") {
+    // set static folder
+    app.use(express.static(path.join(__dirname, "/frontend/build")))
+
+    // any route that is not api will be redirected to index.html
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    })
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running....")
+    })
+}
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(
